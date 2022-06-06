9198ac1530c9d13a25a52994567216944ea1a229
MarkBrianKariuki <markbrian.karanja@student.moringaschoool.com>
2022-06-06 21:21:57 +0300

    commit tree 7b39a5fef6081f77db230113442deb14a5da1c89

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toLocaleUpperCase()}!`;
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
